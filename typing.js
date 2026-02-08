ed3ef75f2bd6e082ffb9db6cd63d6e846b1c33fd
choic097-blip <choic097@newschool.edu>
2026-02-08 17:04:51 -0500

    Update typing.js

--- typing.js
+++ typing.js
@@ -1,40 +1,19 @@
-const words = [
-  "a multi-content creator",
-  "a game developer",
-  "a web designer",
-  "an artist",
-  "a story director",
-  "an author"
-];
+/* typing.js */
 
-let i = 0;
-let j = 0;
-let isDeleting = false;
-let speed = 120;
-
-function type() {
-  const current = words[i];
-
-  if (!isDeleting) {
-    j++;
-  } else {
-    j--;
-  }
-
-  document.getElementById("typing").textContent = current.substring(0, j);
-
-  if (!isDeleting && j === current.length) {
-    isDeleting = true;
-    speed = 900;
-  } else if (isDeleting && j === 0) {
-    isDeleting = false;
-    i = (i + 1) % words.length;
-    speed = 150;
-  } else {
-    speed = isDeleting ? 50 : 120;
-  }
-
-  setTimeout(type, speed);
-}
-
-type();
+var typed = new Typed('#typed', {
+    strings: [
+        "a multi-content creator.",
+        "a game developer.",
+        "a web designer.",
+        "an artist.",
+        "a story director.",
+        "an author."
+    ],
+    typeSpeed: 50,    // 글자 찍는 속도 (작을수록 빠름)
+    backSpeed: 30,    // 지우는 속도
+    backDelay: 1000,  // 다 찍고나서 지우기 전 대기 시간
+    startDelay: 500,  // 시작 전 대기 시간
+    loop: true,       // 무한 반복
+    showCursor: true, // 커서 깜빡임 보이기
+    cursorChar: '|',  // 커서 모양
+});
